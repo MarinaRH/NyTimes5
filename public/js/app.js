d9dea7cac736310f22386981e96f2b076cd6dd1c
MarinaRH <l_marina1717@hotmail.com>
2018-02-14 02:54:07 -0500

    modificando identacion

--- public/js/app.js
+++ public/js/app.js
@@ -14,51 +14,51 @@ btnXhr.addEventListener('click',function(e){
 
 // AJAX con xhr
 function getNews(){
-    const XHR=new XMLHttpRequest();
-    XHR.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`);
-    XHR.onload=addNews;
-    XHR.onerror=handleError;
-    XHR.send();
+  const XHR=new XMLHttpRequest();
+  XHR.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`);
+  XHR.onload=addNews;
+  XHR.onerror=handleError;
+  XHR.send();
 }
 
 function handleError(){
-    console.log('se ha presentado un error');
+  console.log('se ha presentado un error');
 }
 
 function addNews(){
-    const data = JSON.parse(this.responseText);
-    const article = data.response.docs[0];
-    const title = article.headline.main;
-    const snippet = article.snippet;
-    let li = document.createElement('li');
-    li.className = 'articleClass';
-    li.innerText=snippet;
-    responseContainer.appendChild(li);
-    // const response=data.response;
-    // console.log(response);
+  const data = JSON.parse(this.responseText);
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerText=snippet;
+  responseContainer.appendChild(li);
+  // const response=data.response;
+  // console.log(response);
 }
 
 
 // Para AJAX  con fetch
 btnFetch.addEventListener('click', function(e){
-   e.preventDefault();
-   responseContainer.innerHTML='';
-   searchedForText=searchField.value;
-    let uri = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`;
-     fetch(uri)
-     .then(function(response){
-        //  console.log(response);
-         return response.json();
-     }).then(function(data){
-        const article = data.response.docs[0];
-        const title = article.headline.main;
-        const snippet = article.snippet;
-        let li = document.createElement('li');
-        li.className = 'articleClass';
-        li.innerText=snippet;
-        responseContainer.appendChild(li);
+  e.preventDefault();
+  responseContainer.innerHTML='';
+  searchedForText=searchField.value;
+  let uri = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`;
+    fetch(uri)
+    .then(function(response){
+      //  console.log(response);
+        return response.json();
+    }).then(function(data){
+      const article = data.response.docs[0];
+      const title = article.headline.main;
+      const snippet = article.snippet;
+      let li = document.createElement('li');
+      li.className = 'articleClass';
+      li.innerText=snippet;
+      responseContainer.appendChild(li);
     })
-     .catch(function(error){
-         console.log(error);
-     });
+    .catch(function(error){
+        console.log(error);
+    });
 });
